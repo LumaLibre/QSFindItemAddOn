cbe9cb247ec91329a32a603659238bb2c02524cc
Jsinco <jonah@jsinco.dev>
2026-08-01 03:58:15 -0400

    Replace `SHOP_WORLD` with World#key string

--- src/main/java/io/myzticbean/finditemaddon/handlers/gui/menus/FoundShopsMenu.java
+++ src/main/java/io/myzticbean/finditemaddon/handlers/gui/menus/FoundShopsMenu.java
@@ -664,6 +664,6 @@ public class FoundShopsMenu extends PaginatedMenu {
                 .replace(CustomCmdPlaceholdersEnum.SHOP_LOC_X.value(), Double.toString(shopLoc.getX()))
                 .replace(CustomCmdPlaceholdersEnum.SHOP_LOC_Y.value(), Double.toString(shopLoc.getY()))
                 .replace(CustomCmdPlaceholdersEnum.SHOP_LOC_Z.value(), Double.toString(shopLoc.getZ()))
-                .replace(CustomCmdPlaceholdersEnum.SHOP_WORLD.value(), shopLoc.getWorld().getName());
+                .replace(CustomCmdPlaceholdersEnum.SHOP_WORLD.value(), shopLoc.getWorld().key().asString());
     }
 }
